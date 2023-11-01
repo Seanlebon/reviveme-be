8ff9f1515cc86c1fb1d6656f328975ed4b1d3267
Shashank Anand Kannan <shashank.anandkannan@stackadapt.com>
2023-11-01 00:26:08 -0400

    Fix it

--- server.py
+++ server.py
@@ -2,17 +2,20 @@ from flask import Flask, send_from_directory
 import os
 app = Flask(__name__)
 
-react_folder = 'src'
-directory = os.getcwd() + f'f{react_folder}/build/static'
+static_directory = os.getcwd() + f'/build/static'
 
 @app.route('/')
 def index():
     '''Landing page'''
-    build_dir = os.getcwd() + f'/{react_folder}/build'
+    build_dir = os.getcwd() + f'/build'
     return send_from_directory(directory=build_dir, path='index.html')
 
 @app.route('/static/<folder>/<file>')
 def static_files(folder, file):
     '''Serves the contents of the static directory'''
     path = os.path.join(folder, file)
-    return send_from_directory(directory=directory, path=path)
+    return send_from_directory(directory=static_directory, path=path)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
