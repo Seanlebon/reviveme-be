23dd91fbe8a2e69020f9e178c783fc8f6eeec48e
SeanLeBon <sean.chen0818@gmail.com>
2023-11-20 01:27:42 -0800

    added a users list route for testing

--- reviveme/api/v1/routes.py
+++ reviveme/api/v1/routes.py
@@ -1,5 +1,5 @@
 from reviveme import db
-from reviveme.models import Thread
+from reviveme.models import Thread, User
 
 from . import bp
 
@@ -15,3 +15,10 @@ def thread_list():
     # TODO: figure out how to serialize our models properly
     threads = db.session.execute(db.select(Thread)).scalars().all()
     return [{"title": thread.title, "content": thread.content} for thread in threads]
+
+
+@bp.route("/users", methods=["GET"])
+def users_list():
+    # TODO: figure out how to serialize our models properly
+    users = db.session.execute(db.select(User)).scalars().all()
+    return [{"username": user.username, "email": user.email} for user in users]
